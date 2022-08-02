b3eb09e52b3a625208f9666dbf98395bdad11cfb
MuhammadFurqanAli <108631144+MuhammadFurqanAli@users.noreply.github.com>
2022-08-03 02:13:53 +0500

    Add files via upload

--- FirstNonRep.java
+++ FirstNonRep.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;  // Import the Scanner class
+
 class FirstNonRep {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter String: ");
+
     String userName = sc.nextLine();  // Read user input
-    int i=0, k=0, j=0;
+    int i=0, k=0;
     int len=userName.length();
     char[] ch = new char[len];
     int[] cnt = new int[len];
@@ -12,15 +14,14 @@ class FirstNonRep {
     	ch[i]=userName.charAt(i);
     }
     for(i=0; i<len; i++) {
-    	for(k=i; k<len; k++) {
-    			if(ch[i]==ch[k]) {
-    				cnt[j]++;
-    			}
-    			if(cnt[j]==0) {
-    				System.out.println(ch[j]);
-    				System.exit(0);
-    			}
-    			j++;
+    	for(k=0; k<len; k++) {
+    		if(ch[i]==ch[k]) {    				
+    				cnt[i]++;		
+    		}
+    	}
+    	if(cnt[i]==1) {
+    		System.out.println("First Non-Repeating Letter is: "+ch[i]);
+    		break;
     	}
     }
   }
